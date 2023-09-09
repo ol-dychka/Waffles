a3a4f886e1a227c527529762f634429ed4b22b45
ol-dychka <ol.dychka@gmail.com>
2023-09-09 12:59:42 -0400

    Content security headers+

--- API/Program.cs
+++ API/Program.cs
@@ -29,7 +29,7 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
     .BlockAllMixedContent()
     .StyleSources(s => s.Self().UnsafeInline().CustomSources("https://fonts.googleapis.com", "style-src"))
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com"))
